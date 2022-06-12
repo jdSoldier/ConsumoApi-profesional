31e29b84ce54df6daf1292cbe6008a09e5b9490f
josuenc <josue.naure.c@gmail.com>
2022-06-12 10:52:10 -0500

    unificando los stash, parte de la clase 09

--- src/main.js
+++ src/main.js
@@ -1,56 +1,57 @@
 const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3/',
+  baseURL: "https://api.themoviedb.org/3/",
   headers: {
-    'Content-Type': 'application/json;charset=utf-8',
+    "Content-Type": "application/json;charset=utf-8",
   },
   params: {
-    'api_key': API_KEY,
+    api_key: API_KEY,
   },
 });
 
 async function getTrendingMoviesPreview() {
-  const { data } = await api('trending/movie/day');
+  const { data } = await api("trending/movie/day");
   const movies = data.results;
 
-  movies.forEach(movie => {
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
-    
-    const movieContainer = document.createElement('div');
-    movieContainer.classList.add('movie-container');
+  movies.forEach((movie) => {
+    const trendingMoviesPreviewList = document.querySelector(
+      "#trendingPreview .trendingPreview-movieList"
+    );
 
-    const movieImg = document.createElement('img');
-    movieImg.classList.add('movie-img');
-    movieImg.setAttribute('alt', movie.title);
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-container");
+
+    const movieImg = document.createElement("img");
+    movieImg.classList.add("movie-img");
+    movieImg.setAttribute("alt", movie.title);
     movieImg.setAttribute(
-      'src',
-      'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+      "src",
+      "https://image.tmdb.org/t/p/w300" + movie.poster_path
     );
 
     movieContainer.appendChild(movieImg);
-    trendingPreviewMoviesContainer.appendChild(movieContainer);
+    trendingMoviesPreviewList.appendChild(movieContainer);
   });
 }
 
 async function getCategegoriesPreview() {
-  const { data } = await api('genre/movie/list');
+  const { data } = await api("genre/movie/list");
   const categories = data.genres;
 
-  categories.forEach(category => {
-    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
-    
-    const categoryContainer = document.createElement('div');
-    categoryContainer.classList.add('category-container');
+  categories.forEach((category) => {
+    const categoriesPreviewList = document.querySelector(
+      "#categoriesPreview .categoriesPreview-list"
+    );
 
-    const categoryTitle = document.createElement('h3');
-    categoryTitle.classList.add('category-title');
-    categoryTitle.setAttribute('id', 'id' + category.id);
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("category-container");
+
+    const categoryTitle = document.createElement("h3");
+    categoryTitle.classList.add("category-title");
+    categoryTitle.setAttribute("id", "id" + category.id);
     const categoryTitleText = document.createTextNode(category.name);
 
     categoryTitle.appendChild(categoryTitleText);
     categoryContainer.appendChild(categoryTitle);
-    previewCategoriesContainer.appendChild(categoryContainer);
+    categoriesPreviewList.appendChild(categoryContainer);
   });
 }
-
-getTrendingMoviesPreview();
-getCategegoriesPreview();
---
+++ src/nodes.js
@@ -0,0 +1,26 @@
+// Sections
+const headerSection = document.querySelector("#header");
+const trendingPreviewSection = document.querySelector("#trendingPreview");
+const categoriesPreviewSection = document.querySelector("#categoriesPreview");
+const genericSection = document.querySelector("#genericList");
+const movieDetailSection = document.querySelector("#movieDetail");
+
+// Lists & Containers
+const searchForm = document.querySelector("#searchForm");
+const trendingMoviesPreviewList = document.querySelector(
+  ".trendingPreview-movieList"
+);
+const categoriesPreviewList = document.querySelector(".categoriesPreview-list");
+const movieDetailCategoriesList = document.querySelector(
+  "#movieDetail .categories-list"
+);
+const relatedMoviesContainer = document.querySelector(
+  ".relatedMovies-scrollContainer"
+);
+
+// Elements
+const headerTitle = document.querySelector(".header-title");
+const arrowBtn = document.querySelector(".header-arrow");
+const headerCategoryTitle = document.querySelector(
+  ".header-title--categoryView"
+);
