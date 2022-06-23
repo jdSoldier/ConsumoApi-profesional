9b4cb840283f0bc3553eda11f34adda4d5e4dc74
josuenc <josue.naure.c@gmail.com>
2022-06-23 10:44:02 -0500

    boton de cargar mas

--- src/main.js
+++ src/main.js
@@ -20,8 +20,14 @@ const lazyLoader = new IntersectionObserver((entries) => {
   });
 });
 
-function createMovies(movies, container, lazyLoad = false) {
-  container.innerHTML = "";
+function createMovies(
+  movies,
+  container,
+  { lazyLoad = false, clean = true } = {}
+) {
+  if (clean) {
+    container.innerHTML = "";
+  }
 
   movies.forEach((movie) => {
     const movieContainer = document.createElement("div");
@@ -116,7 +122,31 @@ async function getTrendingMovies() {
   const { data } = await api("trending/movie/day");
   const movies = data.results;
 
-  createMovies(movies, genericSection);
+  createMovies(movies, genericSection, { lazyLoad: true, clean: true });
+
+  const btnLoadMore = document.createElement("button");
+  btnLoadMore.innerText = "Cargar mas";
+  btnLoadMore.addEventListener("click", getPaginatedTrendingMovies);
+  genericSection.appendChild(btnLoadMore);
+}
+
+let page = 1;
+
+async function getPaginatedTrendingMovies() {
+  page++;
+  const { data } = await api("trending/movie/day", {
+    params: {
+      page,
+    },
+  });
+  const movies = data.results;
+
+  createMovies(movies, genericSection, { lazyLoad: true, clean: false });
+
+  const btnLoadMore = document.createElement("button");
+  btnLoadMore.innerText = "Cargar mas";
+  btnLoadMore.addEventListener("click", getPaginatedTrendingMovies);
+  genericSection.appendChild(btnLoadMore);
 }
 
 async function getMovieById(id) {
